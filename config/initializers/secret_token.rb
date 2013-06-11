cb4ccc987816060a57da523df4efbaad803a7c3c
Maksim Ind <fss92@pochta.ru>
2013-06-11 11:28:56 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AttorneyIs::Application.config.secret_token = 'cbe924afdf3e8bbfe33a0a407c37ae1055789bdcbf300855cc22677412bee9247ada954c8cc4fc88b26ec23d2c23193bc13aaa07b70705f87c4e3a58bf81c7e1'
