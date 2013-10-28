6b25c6a67ffb85f43cac8fddbe7bbc6325af7efa
Mark Titov <titov.mark@gmail.com>
2013-10-28 11:26:24 +0400

    table width change

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(:version => 20131024111900) do
   add_index "roles", ["name"], :name => "index_roles_on_name"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                              :default => "", :null => false
+    t.string   "email",                                :default => "", :null => false
     t.string   "encrypted_password",                   :default => ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(:version => 20131024111900) do
     t.string   "last_sign_in_ip"
     t.string   "fio"
     t.datetime "birthdate"
-    t.string   "register_number" #номер реестра
+    t.string   "register_number"
     t.integer  "phone"
     t.string   "address"
     t.boolean  "admin"
@@ -101,7 +101,7 @@ ActiveRecord::Schema.define(:version => 20131024111900) do
     t.string   "avatar"
     t.datetime "start_access"
     t.datetime "end_access"
-    t.integer  "number_certificate" #номер удостоверения
+    t.integer  "number_certificate"
     t.string   "firstname"
     t.string   "secondname"
     t.string   "lastname"
