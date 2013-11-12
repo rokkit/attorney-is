66c57886885f00990601849a21396af5f306a2ce
Maxim Indykov <indmaksim@gmail.com>
2013-11-12 18:59:33 +0400

    add smsc

--- app/services/sms.rb
+++ app/services/sms.rb
@@ -4,11 +4,20 @@ class Sms
   end
 
   def self.inform phone, message
-      client = Twilio::REST::Client.new(APP['twilio']['sid'], APP['twilio']['token'])
-      client.account.sms.messages.create(
-        from: APP['twilio']['from'],
-        to: "+#{phone}",
-        body: message
-      )
+      # client = Twilio::REST::Client.new(APP['twilio']['sid'], APP['twilio']['token'])
+      # client.account.sms.messages.create(
+      #   from: APP['twilio']['from'],
+      #   to: "+#{phone}",
+      #   body: message
+      # )
+      post_data = Net::HTTP.post_form URI.parse('http://smsc.ru/sys/send.php'),
+       { 
+         'login' => 'homeinfo',
+         'psw' => 'h54fd54gfd88',
+         'phones' => phone,
+         'mes' => message,
+         'sender' => 'АП СПБ',
+         'charset' => 'utf-8'
+       }
   end
 end
